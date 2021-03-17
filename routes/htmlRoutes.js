a18c7b1e44dbeacb02cbde834cd4f717aff73908
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-03-16 21:46:09 -0400

    change folders

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,23 @@
-  
+
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
 
 
+
 module.exports = function (app) {
 
+    //if * is after words then that means it will be directed to out home page
+
     app.get("*", (req, res) => {
-     return res.sendFile(path.join(__dirname, "../index.html"));
+     return res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     
     //gets notes
     app.get("/notes", (req, res) => {
   
      //retreivee notes
-      return res.sendFile(path.join(__dirname, "../notes.html"));
+      return res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
   
   };
