0a2a8f14eb05bfee78b3e9eda3dba6772ab99ede
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-03-19 11:35:42 -0400

    adding in someting in api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,20 +7,44 @@ module.exports = function(app) {
     // get request for the notes
   
     app.get("/api/notes", function(req, res) {
-        return res.json(mynotes);
+
+      //get old notes 
+        let oldNote = fs.readFileSync(mynotes)
+        
+        return res.json(JSON.parse(oldNote));
     });
   
 
     // post notes 
   
     app.post("/api/notes", function(req, res) {
-   
-        var newNote = req.body;
-        mynotes.push(newNote);
-        res.json(mynotes);
+
+        console.log("my notes", req.body);
+        //stores the notes in an object 
+        let addNotes = {
+          //adds in title to the object based on what was entered 
+          title:req.body.title,
+          //adds in title to the object based on what was entered 
+          Text:req.body.Text
+        };
+        
+        let pastNote = fs.readFileSync(mynotes)
+        pastNote.push(addNotes)
+
+        console.log("my notes", addNotes);
+        console.log("my notes", pastNote);
+
+        //writes the notes 
+        fs.writeFileSync(mynotes,JSON.stringify(pastNote));
+        // mynotes.push(newNote);
+
+        // return res.json(mynotes,newNote);
+
+
     });
   
     app.get("/api/note/:id", function(req, res) {
+      
         var chosen = req.params.character;
       
         console.log(chosen);
