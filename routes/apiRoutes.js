25d858de459ff9707ab23d47c621528998144497
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-03-20 13:49:32 -0400

    fixed format

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,58 +10,59 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
 
       //get old notes 
-        let oldNote = fs.readFileSync(mynotes)
+        let oldNote = fs.readFileSync(JSON.stringify(mynotes))
+        console.log("my old notes", oldNote)
         
         return res.json(JSON.parse(oldNote));
     });
   
 
-    // post notes 
+    // // post notes 
   
-    app.post("/api/notes", function(req, res) {
-       let pastNote = fs.readFileSync(mynotes)
+    // app.post("/api/notes", function(req, res) {
+    //    let pastNote = fs.readFileSync(JSON.stringify(mynotes))
 
-        console.log("my notes", req.body);
-        //stores the notes in an object 
-        let addNotes = {
-          //adds in title to the object based on what was entered 
-          title:req.body.title,
-          //adds in title to the object based on what was entered 
-          Text:req.body.Text,
-          //gives note a unique number
-          id: pastNote.length+1 
+    //     console.log("my notes", req.body);
+    //     //stores the notes in an object 
+    //     let addNotes = {
+    //       //adds in title to the object based on what was entered 
+    //       title:req.body.title,
+    //       //adds in title to the object based on what was entered 
+    //       Text:req.body.Text,
+    //       //gives note a unique number
+    //       id: pastNote.length+1 
 
 
-        };
+    //     };
         
-        // let pastNote = fs.readFileSync(mynotes)
-        pastNote.push(addNotes)
+    //     // let pastNote = fs.readFileSync(mynotes)
+    //     pastNote.push(addNotes)
 
-        console.log("my notes", addNotes);
-        console.log("my notes", pastNote);
+    //     console.log("my notes", addNotes);
+    //     console.log("my notes", pastNote);
 
-        //writes the notes 
-        fs.writeFileSync(mynotes,JSON.stringify(pastNote));
-        // mynotes.push(newNote);
+    //     //writes the notes 
+    //     fs.writeFileSync(mynotes,JSON.stringify(pastNote));
+    //     // mynotes.push(newNote);
 
-        // return res.json(mynotes,newNote);
+    //     // return res.json(mynotes,newNote);
 
 
-    });
+    // });
   
-    app.delete("/api/note/:id", function(req, res) {
+    // app.delete("/api/note/:id", function(req, res) {
 
-        let id = req.params.id;
-        console.log(id);
+    //     let id = req.params.id;
+    //     console.log(id);
 
-        let deletNote = JSON.parse(fs.readFileSync(mynotes));
+    //     let deletNote = JSON.parse(fs.readFileSync(mynotes));
 
-        //sort the info :
-        let deltedList = deletNote.filter((note) => note.id !== id);
+    //     //sort the info :
+    //     let deltedList = deletNote.filter((note) => note.id !== id);
 
-        //writes the notes 
-        fs.writeFileSync(mynotes,JSON.stringify(deltedList));
-    });
+    //     //writes the notes 
+    //     fs.writeFileSync(mynotes,JSON.stringify(deltedList));
+    // });
   
    
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,21 +5,23 @@ const path = require("path");
 
 
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-    
     //gets notes
     app.get("/notes", (req, res) => {
-  
-     //retreivee notes
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+
+        // app.use(express.static(path.join(__dirname, 'public')));
+        //retreivee notes html
+        return res.sendFile(path.join(__dirname,"../public/notes.html"));
+        // return res.sendFile(path.join(__dirname,"../public/notes.html"));
     });
 
      //if * is after words then that means it will be directed to out home page
-
      app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+
+        return res.sendFile(path.join(__dirname,"../public/index.html"));
+
+        // console.log("my path is ", path.join(__dirname, "./public/index.html"));
     });
     
-  
-  };
+};
