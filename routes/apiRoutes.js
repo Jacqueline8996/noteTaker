f72d79b9b3c5840f510b5ddd1efcbc50792cf210
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-03-20 14:03:17 -0400

    added information got the get route working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,46 @@
 //require the old data
 var mynotes = require("../db/db.json");
+//connects to the the database
+var dataBase ='./db/db.json';
+let oldNote = fs.readFileSync(dataBase)
 const fs =require("fs")
 
 
 module.exports = function(app) {
   
-    // get request for the notes
-  
+    // get request for the notes that have been stored
     app.get("/api/notes", function(req, res) {
 
       //get old notes 
-        let oldNote = fs.readFileSync(JSON.stringify(mynotes))
+        // let oldNote = fs.readFileSync(dataBase)
         console.log("my old notes", oldNote)
-        
         return res.json(JSON.parse(oldNote));
     });
   
 
-    // // post notes 
+    // post notes 
   
-    // app.post("/api/notes", function(req, res) {
-    //    let pastNote = fs.readFileSync(JSON.stringify(mynotes))
+    app.post("/api/notes", function(req, res) {
+      //  let pastNote = fs.readFileSync(JSON.stringify(mynotes))
 
-    //     console.log("my notes", req.body);
-    //     //stores the notes in an object 
-    //     let addNotes = {
-    //       //adds in title to the object based on what was entered 
-    //       title:req.body.title,
-    //       //adds in title to the object based on what was entered 
-    //       Text:req.body.Text,
-    //       //gives note a unique number
-    //       id: pastNote.length+1 
+        console.log("my notes", req.body);
+        //stores the notes in an object 
+        let addNotes = {
+          //adds in title to the object based on what was entered 
+          title:req.body.title,
+          //adds in title to the object based on what was entered 
+          Text:req.body.Text,
+          //gives note a unique number
+          id: pastNote.length+1 
 
 
-    //     };
+        };
         
-    //     // let pastNote = fs.readFileSync(mynotes)
-    //     pastNote.push(addNotes)
+        // let pastNote = fs.readFileSync(mynotes)
+        oldNote.push(addNotes)
 
-    //     console.log("my notes", addNotes);
-    //     console.log("my notes", pastNote);
+        console.log("my notes", addNotes);
+        // console.log("my notes", pastNote);
 
     //     //writes the notes 
     //     fs.writeFileSync(mynotes,JSON.stringify(pastNote));
@@ -48,7 +49,7 @@ module.exports = function(app) {
     //     // return res.json(mynotes,newNote);
 
 
-    // });
+    });
   
     // app.delete("/api/note/:id", function(req, res) {
 
